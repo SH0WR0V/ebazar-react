e9fab298bd306ea0ec070450b78a4f60cf8807b1
SH0WR0V <sm.shahriar1230@gmail.com>
2024-06-25 22:05:23 +0600

    updated changes

--- src/pages/Cart/components/CartCard.js
+++ src/pages/Cart/components/CartCard.js
@@ -1,6 +1,6 @@
+import { useState, useEffect } from "react";
 import { Link } from "react-router-dom";
 import { useCart } from "../../../context";
-import { useState, useEffect } from "react";
 
 export const CartCard = ({ product }) => {
     const { updateQuantity, removeFromCart } = useCart();
