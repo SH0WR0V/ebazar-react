ab89eadad9131cc59388dc9f73ab071ba25e479f
SH0WR0V <sm.shahriar1230@gmail.com>
2024-06-29 01:27:19 +0600

    netlify update

--- src/index.js
+++ src/index.js
@@ -4,10 +4,8 @@ import { BrowserRouter as Router } from "react-router-dom";
 
 import { ToastContainer } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
-
 import { ScrollToTop } from "./components";
 import { FilterProvider, CartProvider } from "./context";
-
 import "./index.css";
 import App from "./App";
 
